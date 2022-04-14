571ae60675e8be9e5be4de5116bde6766d3653c6
Aya74 <alarayshi.aya2002@gmail.com>
2022-04-14 22:01:11 +0300

    Create the navbar section using html, un ordered list, with 5 links for: personal information page, course information, experience information page, add course page and add experience. Add the logo of the page, and at last prepare the header section on each page. relates #5

---
+++ PHP/AddCourse.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV Website</title>
+    <link rel="stylesheet" href="../CSS/MyStyle.css">
+</head>
+<body>
+    <!-- start header section -->
+    <div class="header">
+        <!-- start navbar section -->
+        <div class="navbar">
+            <!-- navbar links -->
+            <div class="nav">
+                <div class="list">
+                    <li  class="list-item "><a href="Home.php" class="link-nav ">personal information</a></li>
+                    <li class="list-item "><a href="ViewCourses.php" class="link-nav ">course information</a></li>
+                    <li class="list-item "><a href="ViewExperience.php" class="link-nav">experience information</a></li>
+                    <li class="list-item "><a href="AddCourse.php" class="link-nav current">add course</a></li>
+                    <li class="list-item "><a href="AddExperience.php" class="link-nav">add experience</a></li>
+                </div>
+            </div>
+             <!-- nav bar logo -->
+          <div class="logo">
+              <img src="../Images/azharLogo.png" alt="Al Azhar logo" id="alazharLogo"/>
+            </div>
+        </div>
+        <!-- end navbar section -->
+        
+        <!-- start header content section -->
+        <div class="header-content">
+      <div class="header-content-item">
+      </div>
+      </div>
+    </div>
+    <!-- end header section -->
+</body>
+</html>
