1604bb623251a92cb9619a57b5d3511082815271
Aya74 <alarayshi.aya2002@gmail.com>
2022-04-27 21:49:20 +0300

    Create the structure of Add Course Page Using html code, adding(A form that contains (input type text for Course name, input type number for Number of Hours, input type date for Start date, input type date for End date, input type text for institution, input type radio for Attachment, input type url for URL Attachment, input type file for file Attachment, text area for some notes, input to button to save form information,   input to button to reset form) and An image. relates #9

--- PHP/AddCourse.php
+++ PHP/AddCourse.php
@@ -31,9 +31,61 @@
         
         <!-- start header content section -->
         <div class="header-content">
-      <div class="header-content-item">
-      </div>
-      </div>
+        <div class="header-content-item">
+            <div class="row">
+                <form action="#" method="post" id="addCourseForm">
+                    <table id="addCourseTable">
+                        <tr class="addCourseTableRow">
+                            <td><label for="courseName">Course Name:</label></td>
+                            <td><input type="text" name="courseName" id="courseName"></td>
+                        </tr>
+                        <tr class="addCourseTableRow">
+                            <td><label for="hoursNum">Number of Hours:</label></td>
+                            <td> <input type="number" name="hoursNum" id="hoursNum"></td>
+                        </tr>
+                        <tr class="addCourseTableRow">
+                            <td><label for="startDate">Start Date:</label></td>
+                            <td> <input type="date" name="startDate" id="startDate"></td>
+                        </tr>
+                        <tr class="addCourseTableRow">
+                            <td><label for="endtDate">End Date:</label></td>
+                            <td> <input type="date" name="endtDate" id="endtDate"></td>
+                        </tr>
+                        <tr class="addCourseTableRow">
+                            <td><label for="institution">Institution:</label></td>
+                            <td><input type="text" name="institution" id="institution"></td>
+                        </tr>
+                        <tr class="addCourseTableRow">
+                            <td><label for="attachment">Attachment:</label></td>
+                            <td id="attachment">
+                                <input type="radio" name="attachment" id="attachment_File" value="File">
+                                <label for="attachment_File">File</label>
+                                <input type="radio" name="attachment" id="attachment_URL" value="URL">
+                                <label for="attachment_URL">URL</label>
+                            </td>
+                        </tr>
+                        <tr class="addCourseTableRow">
+                            <td><label for="urlInput">URL:</label></td>
+                            <td><input type="url" name="urlInput" id="urlInput"></td>
+                        </tr>
+                        <tr class="addCourseTableRow">
+                            <td><label for="fileInput">File:</label></td>
+                            <td><input type="file" name="fileInput" id="fileInput"></td>
+                        </tr>
+                        <tr class="addCourseTableRow">
+                            <td><label for="notes">Notes:</label></td>
+                            <td><textarea name="notes" id="notes" cols="25" rows="8"></textarea></td>
+                        </tr>
+                        <tr class="addCourseTableRow">
+                            <td><input type="submit" value="Save"></td>
+                            <td><input type="reset" value="Reset"></td>
+                        </tr>
+                    </table>
+                </form>
+                <img src="../Images/course.jpg" alt="Course Image">
+            </div>
+        </div>
+        </div>
     </div>
     <!-- end header section -->
 </body>
