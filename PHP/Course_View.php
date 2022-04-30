09a8c909f628ac4ae51e0e4d7b0ff6977b624be7
Aya74 <alarayshi.aya2002@gmail.com>
2022-04-30 12:27:02 +0300

    Create the navbar section on Course View page thant consist of 5 links for 5 pages, logo and styled with back-ground color blue relates #19

--- PHP/Course_View.php
+++ PHP/Course_View.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,7 +9,30 @@
     <!-- custome css style -->
     <link rel="stylesheet" href="../CSS/MyStyle.css">
 </head>
+
 <body>
-    
+    <!-- start header section -->
+    <div class="header">
+        <!-- start navbar section -->
+        <div class="navbar">
+            <!-- navbar links -->
+            <div class="nav">
+                <div class="list">
+                    <li class="list-item "><a href="Home.php" class="link-nav">personal information</a></li>
+                    <li class="list-item "><a href="ViewCourses.php" class="link-nav">course information</a></li>
+                    <li class="list-item "><a href="ViewExperience.php" class="link-nav">experience information</a></li>
+                    <li class="list-item "><a href="AddCourse.php" class="link-nav">add course</a></li>
+                    <li class="list-item "><a href="AddExperience.php" class="link-nav">add experience</a></li>
+                </div>
+            </div>
+            <!-- nav bar logo -->
+            <div class="logo">
+                <img src="../Images/azharLogo.png" alt="Al Azhar logo" id="alazharLogo" />
+            </div>
+        </div>
+        <!-- end navbar section -->
+    </div>
+    <!-- end header section -->
 </body>
+
 </html>
