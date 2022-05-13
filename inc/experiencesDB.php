a9149de326f4069722c7c6d8a31b95f6a677254f
Aya74 <alarayshi.aya2002@gmail.com>
2022-05-13 18:43:31 +0300

    Check if the connection failed or success, if failed send error msg relates #23

--- inc/experiencesDB.php
+++ inc/experiencesDB.php
@@ -1,5 +1,8 @@
 <?php
 //var to connect database
 $conn = mysqli_connect('localhost','root','root','cvwebsite');
+//if the connection fail display error msg
+if(!$conn)
+echo 'Error: '.mysqli_connect_error();
 
 ?>
