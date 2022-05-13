33a2fbc6a39087c1176e2c058374997156915e76
Aya74 <alarayshi.aya2002@gmail.com>
2022-05-13 18:44:28 +0300

    Create var for each input relates #23

--- inc/experiencesDB.php
+++ inc/experiencesDB.php
@@ -4,5 +4,15 @@ $conn = mysqli_connect('localhost','root','root','cvwebsite');
 //if the connection fail display error msg
 if(!$conn)
 echo 'Error: '.mysqli_connect_error();
+//variables declarpation
+// if statment to fix waring error (Undefined array key)
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $experiencesCategory = $_POST['experiencesCategory'];
+    $experiencesTitle = $_POST['experiences'];
+    $startMonth = $_POST['startMonth'];
+    $endMonth = $_POST['endMonth'];
+    $institution = $_POST['institution'];
+    $descriptionCourse = $_POST['description'];
+    }
 
 ?>
