3111f6e9c04133faa088dc93dd7172dddb797d5d
Aya74 <alarayshi.aya2002@gmail.com>
2022-05-15 23:00:33 +0300

    Add connection to the database and display error msg if the connection fails relates #25

--- PHP/AddCourse.php
+++ PHP/AddCourse.php
@@ -1,3 +1,10 @@
+<?php
+//var to connect database
+$conn = mysqli_connect('localhost','root','root','cvwebsite');
+//if the connection fail display error msg
+if(!$conn)
+echo 'Error: '.mysqli_connect_error();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
